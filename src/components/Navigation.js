e700cd92f0c0fe10f4c5b0b3e8ca6964a8be13ae
Clara Forrestal <clara.forrestal@levvel.io>
2021-09-10 13:22:35 -0400

    no nav links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,8 +6,8 @@ function Navigation(){
         <nav class="navbar" data-component="Navigation">
             <ul>
                 <li><a href="index.html">Home</a></li>
-                <li><a href="my-work.html">My Work</a></li>
-                <li><a href="experience.html">Experience</a></li>
+                {/* <li><a href="my-work.html">My Work</a></li>
+                <li><a href="experience.html">Experience</a></li> */}
             </ul>
             <button class="primary-button" data-component="Primary-Button"><a href="assets/Resume.pdf" download="ClaraForrestalResume.pdf">Download Resume</a></button>
         </nav>
