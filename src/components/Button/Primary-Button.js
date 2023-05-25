aa1e084a39b2d871848e3ed4d4a2ddd0da1091e1
clara forrestal <cforrestal@WLX4HTJ6CR.local>
2023-05-25 00:03:13 -0400

    profile hero v1

--- src/components/Button/Primary-Button.js
+++ src/components/Button/Primary-Button.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './Primary-Button.scss';
+import Download from "../../assets/icons/arrow-down-to-line.png";
 
 function PrimaryButton(){
     return (
         <button className="primary-button">
             Download Resume
+            <img alt="" src={Download} />
         </button>
     );
 }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -11,15 +11,17 @@ function Profile() {
             <div className="hero-wrapper">
                 <div className="hero">
                     <img src={Avatar} alt=""/>
-                    <div className="hero-text">
-                        <h1>I’m Clara Forrestal</h1>
-                        <h2>Senior UX Designer & DesignOps Consultant</h2>
-                        <div className="description">
-                            <p>I strive to create exceptional experiences not only for end-users, but also for my colleagues and clients. By eliminating friction across teams, optimizing processes, and establishing practical systems, I actively contribute to the success of my team and the creation of world-class digital products.</p>
-                            <p>I started my career as a front-end developer, building interfaces in React and CSS for my clients. After about a year, I discovered UX and quickly fell in love. Since then, I have been eagerly exploring every facet of the field. Over the past five years, I have evolved into a versatile senior designer, sharing my knowledge and guiding others along the way.</p>
-                            <p>Today, my expertise spans a wide range of UX disciplines, with a focus on design systems, <a href="https://ooux.com/">Object-Oriented UX</a>, DesignOps, and connecting design and development teams.</p>
+                    <div className="right">
+                        <div className="hero-text">
+                            <h1>I’m Clara Forrestal</h1>
+                            <h2>Senior UX Designer & DesignOps Consultant</h2>
+                            <div className="description">
+                                <p>I strive to create exceptional experiences not only for end-users, but also for my colleagues and clients. By eliminating friction across teams, optimizing processes, and establishing practical systems, I actively contribute to the success of my team and the creation of world-class digital products.</p>
+                                <p>I started my career as a front-end developer, building interfaces in React and CSS for my clients. After about a year, I discovered UX and quickly fell in love. Since then, I have been eagerly exploring every facet of the field. Over the past five years, I have evolved into a versatile senior designer, sharing my knowledge and guiding others along the way.</p>
+                                <p>Today, my expertise spans a wide range of UX disciplines, with a focus on design systems, <a href="https://ooux.com/">Object-Oriented UX</a>, DesignOps, and connecting design and development teams.</p>
+                            </div>
+                            <PrimaryButton/>
                         </div>
-                        <PrimaryButton/>
                     </div>
                 </div>
             </div>
