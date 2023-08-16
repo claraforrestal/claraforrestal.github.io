cdd24dd49e72526c83c772d56d92e79237226ede
clara forrestal <cforrestal@WLX4HTJ6CR.local>
2023-08-16 16:11:18 -0400

    visual fixes

--- src/components/Button/Primary-Button.js
+++ src/components/Button/Primary-Button.js
@@ -1,12 +1,10 @@
 import React from "react";
 import './Primary-Button.scss';
-import Download from "../../assets/icons/arrow-down-to-line.png";
 
 function PrimaryButton(props){
     return (
         <a className="primary-button" href={props.href} download>
             <span className="primary-button-label">{props.text}</span>
-            <img alt="" src={Download} />
         </a>
     );
 }
