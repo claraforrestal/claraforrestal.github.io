501d0ae0ef8714c4c8af3e3559fc4cd6fc2d13e9
clara forrestal <cforrestal@WLX4HTJ6CR.local>
2023-08-17 11:57:54 -0400

    links

--- src/components/PresentationCard/Presentation-Card.js
+++ src/components/PresentationCard/Presentation-Card.js
@@ -3,15 +3,17 @@ import './Presentation-Card.scss';
 
 function PresentationCard(props) {
     return (
-        <div className='presentation-card'>
-            <h3 className="title">{props.title}</h3>
-            <div className='metadata'>
-                <span className='date'>{props.date}</span>
-                <span className='divider'></span>
-                <span className='topic'>{props.topic}</span>
+        <a className="presentation-card-link" href={props.link} target="_blank" >
+            <div className='presentation-card'>
+                <h3 className="title">{props.title}</h3>
+                <div className='metadata'>
+                    <span className='date'>{props.date}</span>
+                    <span className='divider'></span>
+                    <span className='topic'>{props.topic}</span>
+                </div>
+                {props.description && (<p>{props.description}</p>)}
             </div>
-            { props.description && (<p>{props.description}</p>)}
-        </div>
+        </a>
     );
 }
 
--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -38,14 +38,16 @@ function Profile() {
                 <h5>Recent presentations</h5>
                 <div className='presentation-cards'>
                     <PresentationCard
+                        link="https://www.figma.com/proto/DUtCK4076Xdl0mhaWtoJ6D/Presentation%3A-How-to-tidy-up-your-Design-System?page-id=15%3A2&type=design&node-id=15-3&viewport=15536%2C489%2C0.5&t=hgRMVd6RaaH5wm4s-8&scaling=contain&starting-point-node-id=15%3A3&hide-ui=1"
                         title="How to tidy up your design system like Marie Kondo"
-                        date="12 Feb 2020"
+                        date="July 2022"
                         topic="Design Systems"/>
                     <PresentationCard
+                        link="https://www.figma.com/proto/4Issal4xPA5PKX4Y1xPI42/Presentation%3A-The-Four-Horsemen-of-Poor-Usability?page-id=15%3A2&type=design&node-id=301-3&viewport=-940%2C0%2C0.06&t=CbdYBoBdObF5u9ev-8&scaling=contain&starting-point-node-id=301%3A17&hide-ui=1"
                         title="Object-Oriented UX:
                             The four horsemen
                             of poor usability"
-                        date="12 Feb 2020"
+                        date="July 2023"
                         topic="OOUX"/>
                 </div>
             </div>
